8b76c5107718695a64fb552bf462ea6ce80f9581
hasansharif819 <hs.sharif819@gmail.com>
2022-05-09 23:02:26 +0600

    update api

--- index.js
+++ index.js
@@ -10,24 +10,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//JWT
-// function varifyJWT(req, res, next) {
-//     const authHeader = req.headers.authorization;
-//     console.log(authHeader);
-//     if (!authHeader) {
-//         return res.status(401).send({ message: 'Unauthorized Access' });
-//     }
-//     const token = authHeader.split(' ')[1];
-//     jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
-//         if (err) {
-//             return res.status(403).rsend({ message: 'Forbidden Access' });
-//         }
-//         console.log('decoded', decoded);
-//         req.decoded = decoded;
-//         next();
-//     })
-// }
-
 // mongo db 
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.natc4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -54,8 +36,8 @@ async function run(){
         app.get('/inventory/:id', async(req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const inventory = await smartColletion.findOne(query);
-            res.send(inventory);
+            const result = await smartColletion.findOne(query);
+            res.send(result);
         });
         //Delete from database
         app.delete('/inventory/:id', async (req, res) => {
@@ -64,6 +46,25 @@ async function run(){
             const result = await smartColletion.deleteOne(query);
             res.send(result);
         });
+        //Update
+        app.put('/inventory/:id', async(req, res) => {
+            const id = req.params.id;
+            const updateProduct = req.body;
+            const filter = {_id: ObjectId(id)};
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    name: updateProduct.name,
+                    price: updateProduct.price,
+                    supplier: updateProduct.supplier,
+                    quantity: updateProduct.quantity,
+                    description: updateProduct.description,
+                    img: updateProduct.img
+                }
+            };
+            const result = await smartColletion.updateOne(filter, updateDoc, options);
+            res.send(result);
+        })
 
         //post // form or data received from client server
         app.post('/inventory', async (req, res) => {
@@ -88,27 +89,16 @@ async function run(){
             const email = req.query.email;
             const query = { email };
             const cursor = myItemCollection.find(query);
-            const items = await cursor.toArray();
-            res.send(items);
-        })
+            const result = await cursor.toArray();
+            res.send(result);
+        });
 
-        // app.get('/myitem', varifyJWT, async(req, res) => {
-        //     const email = req.query.email;
-        //     console.log(email);
-        //     const decodedEmail = req.decoded.email;
-        //     // const email = req.query.email;
-        //     if(decodedEmail === email){
-        //         const query = { email };
-        //         const cursor = myItemCollection.find(query);
-        //         const result = await cursor.toArray();
-        //         res.send(result);
-        //         console.log(decodedEmail)
-        //     }
-        //     else{
-        //         return req.status(403).send({message: 'Forbidden Access'});
-        //         console.log('whats happened??')
-        //     }
-        // })
+        app.delete('/myitem/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await myItemCollection.deleteOne(query);
+            res.send(result);
+        });
     }
     finally{
 
